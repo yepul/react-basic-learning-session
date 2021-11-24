fd68a8fdad764c3da55665820132291e202471b3
Farid <fnikman@gmail.com>
2021-11-24 15:03:53 +0800

    feat: pass query

--- src/hooks/useCapsules.ts
+++ src/hooks/useCapsules.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 type TMissions = {
   name: string;
@@ -18,18 +18,47 @@ interface ISpaceXResponse {
   reuse_count: number;
 }
 
-export const useCapsules = () => {
+type TQuery = {
+  capsule_serial?: string;
+  capsule_id?: string;
+  status?: string;
+  original_launch?: string;
+  mission?: string;
+  landings?: number;
+  type?: string;
+  reuse_count?: number;
+};
+
+export const useCapsules = (params: TQuery) => {
+  /**
+   * {
+   *     capsule_id: 'dragon1',
+   *     status: 'unknown'
+   * }
+   *
+   * => ?capsule_id=dragon1&status=unknown
+   */
+
+  const query = useMemo(() => {
+    const resource = Object.entries(params);
+    if (!resource.length) {
+      return "";
+    }
+
+    return "?" + resource.map((source) => source.join("=")).join("&");
+  }, [params]);
+
   const [spaceXCapsulesData, setSpaceXCapsulesData] = useState<
     ISpaceXResponse[] | []
   >(() => []);
 
   useEffect(() => {
-    fetch("https://api.spacexdata.com/v3/capsules")
+    fetch("https://api.spacexdata.com/v3/capsules" + query)
       .then((response) => response.json())
       .then((data: ISpaceXResponse[]) => {
         setSpaceXCapsulesData(data);
       });
-  }, []);
+  }, [query]);
 
   return spaceXCapsulesData;
 };
