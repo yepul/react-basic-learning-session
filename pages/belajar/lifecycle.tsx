03e20540c5e736933c5de048d0a7541b4d3f354d
Farid <fnikman@gmail.com>
2021-11-25 17:54:23 +0800

    feat: SEO

--- pages/belajar/lifecycle.tsx
+++ pages/belajar/lifecycle.tsx
@@ -4,6 +4,8 @@ import { GetServerSideProps } from "next";
 import { HeroBanner } from "@/src/components/HeroBanner";
 import { CapsuleCard } from "@/src/components/CapsuleCard";
 import { Container } from "@/src/components/Container";
+import { PageHeader } from "@/src/components/PageHeader";
+import Head from "next/head";
 
 const Lifecycle: FunctionComponent<{ data: ISpaceXResponse[] }> = (props) => {
   // - [x] lifecycle
@@ -20,29 +22,65 @@ const Lifecycle: FunctionComponent<{ data: ISpaceXResponse[] }> = (props) => {
   const spaceXCapsulesData = useCapsules({}, props.data);
 
   return (
-    <div className="mb-5">
-      {/*// hero banner
-          xs => safe value
-          md => ipad vertical
-          lg => desktop
-      */}
-      <HeroBanner
-        title={
-          <>
-            apa-apa dekat sini, dia feeling.
-            <br />
-            yepul nanti dapat PS5 dia feeling jugak
-          </>
-        }
-      />
-      <Container>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-          {spaceXCapsulesData.map((capsule: ISpaceXResponse) => (
-            <CapsuleCard key={capsule.capsule_serial} {...capsule} />
-          ))}
-        </div>
-      </Container>
-    </div>
+    <>
+      <Head>
+        <title>Siri Mengajar React Lifecycle</title>
+        <meta name="title" content="Siri Mengajar Lifecycle" />
+        <meta
+          name="description"
+          content="siri mengajar mior dan saiful mengenali React lifecycle menggunakan SpaceX Capsule public API"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://www.github.com/mkxhmn/siri-mengajar"
+        />
+        <meta property="og:title" content="Siri Mengajar Lifecycle" />
+        <meta
+          property="og:description"
+          content="siri mengajar mior dan saiful mengenali React lifecycle menggunakan SpaceX Capsule public API"
+        />
+        <meta
+          property="og:image"
+          content="https://source.unsplash.com/k38XYpPLs-U/1200x628"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.github.com/mkxhmn/siri-mengajar"
+        />
+        <meta property="twitter:title" content="Siri Mengajar Lifecycle" />
+        <meta
+          property="twitter:description"
+          content="siri mengajar mior dan saiful mengenali React lifecycle menggunakan SpaceX Capsule public API"
+        />
+        <meta
+          property="twitter:image"
+          content="https://source.unsplash.com/k38XYpPLs-U/1200x628"
+        />
+      </Head>
+      <div className="mb-5">
+        <PageHeader />
+        <HeroBanner
+          title={
+            <>
+              apa-apa dekat sini, dia feeling.
+              <br />
+              yepul nanti dapat PS5 dia feeling jugak
+            </>
+          }
+        />
+        <Container>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+            {spaceXCapsulesData.map((capsule: ISpaceXResponse) => (
+              <CapsuleCard key={capsule.capsule_serial} {...capsule} />
+            ))}
+          </div>
+        </Container>
+      </div>
+    </>
   );
 };
 
